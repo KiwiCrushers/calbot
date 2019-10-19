469d6d9b0ef4dc5337e9ab5cfde1096878c4b66e
brandonsmith-prgrms <53955502+brandonsmith-prgrms@users.noreply.github.com>
2019-10-19 17:13:36 -0400

    added changes to filename in calbot

--- Mods/calbot.py
+++ Mods/calbot.py
@@ -1,9 +1,9 @@
 import requests
 
-def from_url(url):
+def from_url(url, user, id):
     # Retrieve the filetype of the URL.
     filetype = url[-3:]
 
     # Downloading the file into our system.
     r = requests.get(url, allow_redirects=True)
-    open('images/file.' + filetype, 'wb').write(r.content)
+    open('images/' + user + '-' + id + '.' + filetype, 'wb').write(r.content)
