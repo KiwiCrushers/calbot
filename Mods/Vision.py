d2e87857979c876b10ab1eee9e6d6dda939091ee
brandonsmith-prgrms <53955502+brandonsmith-prgrms@users.noreply.github.com>
2019-10-19 15:58:00 -0400

    Added vision

---
+++ Mods/Vision.py
@@ -0,0 +1,13 @@
+def detect_labels(path):
+    from google.cloud import vision
+    import io
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.types.Image(content=content)
+
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    return labels
