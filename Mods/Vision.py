844c8a36c1c1e5a60fc9f3d3753c92903b7296d6
Carlos Saucedo <carlos@cgsphoto.com>
2019-10-19 20:31:57 -0400

    Changed auth method for vision module.

--- Mods/Vision.py
+++ Mods/Vision.py
@@ -1,7 +1,11 @@
 def detect_labels(path):
     from google.cloud import vision
-    import io
-    client = vision.ImageAnnotatorClient()
+    import io, json
+
+    with open("json/config.json") as h:
+	config = json.load(h)
+
+    client = vision.ImageAnnotatorClient.from_service_account_json(config["gcloud"]["tokenFile"])
 
     with io.open(path, 'rb') as image_file:
         content = image_file.read()
